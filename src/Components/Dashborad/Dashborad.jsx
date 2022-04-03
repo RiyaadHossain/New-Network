12d0718cee13428c1e31120450f4d6dcc6d85e08
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-04 03:11:49 +0600

    Dahsboard Added

--- src/Components/Dashborad/Dashborad.jsx
+++ src/Components/Dashborad/Dashborad.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {
-  Area,
-  AreaChart,
+  Bar,
+  BarChart,
   CartesianGrid,
+  Legend,
   Line,
   LineChart,
   Tooltip,
@@ -12,99 +13,42 @@ import {
 
 const Chart = () => {
   const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+    { month: "Mar", investment: 100000, sell: 241, revenue: 10401 },
+    { month: "Apr", investment: 200000, sell: 423, revenue: 24500 },
+    { month: "May", investment: 500000, sell: 726, revenue: 67010 },
+    { month: "Jun", investment: 500000, sell: 529, revenue: 40405 },
+    { month: "Jul", investment: 600000, sell: 601, revenue: 50900 },
+    { month: "Aug", investment: 700000, sell: 670, revenue: 61000 },
   ];
   return (
-    <div className="mx-auto container">
-      <LineChart
+    <div className=" container mx-auto">
+          <LineChart
+              className="mx-auto"
         width={730}
         height={250}
         data={data}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
       >
-        <Line type={"monotone"} dataKey="uv" stroke="#8884d8" />
-        <Line type={"monotone"} dataKey="pv" stroke="#82ca9d" />
+        <Line type={"monotone"} dataKey="revenue" stroke="#8884d8" />
+        <Line type={"monotone"} dataKey="investment" stroke="#82ca9d" />
         <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <XAxis />
+              <Tooltip />
+              <Legend />
+        <XAxis dataKey={'month'} />
         <YAxis />
       </LineChart>
-      <AreaChart
-        width={730}
-        height={250}
-        data={data}
-        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-      >
-        <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-          </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="name" />
-        <YAxis />
+
+      <BarChart className="mt-20 mx-auto" width={730} height={250} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
         <Tooltip />
-        <Area
-          type="monotone"
-          dataKey="uv"
-          stroke="#8884d8"
-          fillOpacity={1}
-          fill="url(#colorUv)"
-        />
-        <Area
-          type="monotone"
-          dataKey="pv"
-          stroke="#82ca9d"
-          fillOpacity={1}
-          fill="url(#colorPv)"
-        />
-      </AreaChart>
+        <Legend />
+        <Bar dataKey="revenue" fill="#8884d8" />
+        <Bar dataKey="investment" fill="#82ca9d" />
+      </BarChart>
+
+      
     </div>
   );
 };
